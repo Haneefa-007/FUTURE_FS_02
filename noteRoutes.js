3d41b7b3324f6765331555cf7cfa1ddc7b3c92f0
Haneefa-007 <007.haneefa.a.b@gmail.com>
2026-05-23 12:56:41 +0530

    Delete noteRoutes.js

--- noteRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addNote, getNotesByLeadId } = require('../controllers/noteController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/', protect, addNote);
-router.get('/lead/:leadId', protect, getNotesByLeadId);
-
-module.exports = router;
